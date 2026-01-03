b194cc4dc5d3306935d6a8e85ff71a0f8106d9ed
Godofredo Nah <skate_forever_1990@hotmail.com>
2026-01-02 23:52:45 -0600

    Punto optimo, implementar refresh tokens

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 /* Express para crear el servidor */
 const express = require('express')
+/* CORS */
+const cors = require('cors')
 /* Rutas */
 const router = express.Router()
 const authRoutes = require('./routes/auth.routes')
 const healthRoutes = require('./routes/health')
 const privateRoutes = require('./routes/private.routes')
-
 /* Para crear el servidor */
 const app = express()
 /* Para parsear el body */
 app.use(express.json())
+/* Para configurar CORS */
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
-app.use('/health', healthRoutes)
-app.use('/auth', authRoutes)
+app.use('/api/health', healthRoutes)
+app.use('/api/auth', authRoutes)
 app.use('/api', privateRoutes)
 
 module.exports = app
